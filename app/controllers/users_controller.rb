69140980476b1459d7a8caa83e5afc69455e0085
Takashi <th2826@columbia.edu>
2022-08-02 19:31:07 -0400

    UsersController#create (signup), UsersController#show (auto-login), SessionsController#create (login) and SessionsController#destroy (logout) are created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,29 @@ class UsersController < ApplicationController
     render json: users
   end
   
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user[:id]
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user, status: :ok
+    else
+      render json: { error: "Not authorize" }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation, :profile_image, :intro, :website, :github)
+  end
+
 end
