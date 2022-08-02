0914f71a6e22a20a23d2b29bd37a95b3430e4117
Takashi <th2826@columbia.edu>
2022-08-02 18:46:25 -0400

    routes for signup, auto-login, login and logout created

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   resources :genres, only: [:index]
   resources :technologies, only: [:index]
 
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   get '*path',
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
