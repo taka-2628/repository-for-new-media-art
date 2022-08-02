e73120a6b430babec709c0b58a3b7df96b17c6d0
Takashi <th2826@columbia.edu>
2022-08-01 21:05:57 -0400

    routes for users, projects, comments, genres, and technologies created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get '/hello', to: 'application#hello_world'
+  get '/hello', to: 'application#hello_world' #route for testing
+
+  resources :users, only: [:index]
+  resources :projects
+  resources :comments, except: [:index, :show]
+  resources :genres, only: [:index]
+  resources :technologies, only: [:index]
 
   get '*path',
       to: 'fallback#index',
