0b3c5bfe087331b00336978230f52a6c3ddd6ad9
Takashi <th2826@columbia.edu>
2022-08-01 22:31:10 -0400

    create, update, delete actions created in CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,30 @@
 class CommentsController < ApplicationController
+
+  def create
+    comment = Comment.create(comment_params)
+    render json: comment, status: :created
+  end
+
+  def update
+    comment = find_comment
+    comment.update(comment_params)
+    render json: comment, status: :accepted
+  end
+
+  def destroy
+    comment = find_comment
+    comment.destroy
+    head :no_content, status: :deleted
+  end
+  
+  private
+
+  def find_comment
+    Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.permit(:user_id, :project_id, :body)
+  end
+
 end
