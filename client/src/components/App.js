9f297cd629d55c2394006d70078048f3537edf17
Takashi <th2826@columbia.edu>
2022-08-04 16:15:52 -0400

    react: components and stylesheets folders created and files moved to respective folders

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import '../stylesheets/App.css';
 
 function App() {
   const [count, setCount] = useState(0);
