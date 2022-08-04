2a923e2b56ee4cbf0f22cb8942cfb5c52d719a02
Takashi <th2826@columbia.edu>
2022-08-03 23:37:39 -0400

    CommentUserSerializer is created and CommentSerializer is updated

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,6 +1,6 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :body, :created_at
 
-  belongs_to :user
+  belongs_to :user, serializer: CommentUserSerializer
   belongs_to :project
 end
---
+++ app/serializers/comment_user_serializer.rb
@@ -0,0 +1,3 @@
+class CommentUserSerializer < ActiveModel::Serializer
+  attributes :id, :username, :profile_image
+end
