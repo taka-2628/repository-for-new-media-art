183958c4c3fae64690053bd0e7b5d438cacf557e
Takashi <th2826@columbia.edu>
2022-08-09 16:03:25 -0400

    login functionality in Submit component created

--- client/src/components/Submit.js
+++ client/src/components/Submit.js
@@ -1,20 +1,65 @@
-import React from "react";
+import React, {useState} from "react";
+import {useNavigate} from 'react-router-dom';
 import "../stylesheets/Submit.css";
 
-import Login from "./Login";
 import ProjectForm from "./ProjectForm";
 
-function Submit( { genres, technologies, currentUser, setCurrentUser, users } ) {  
-  
-  
+function Submit( { genres, technologies, currentUser, setCurrentUser } ) {  
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSubmit(e){
+    e.preventDefault();
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => {
+          setCurrentUser(user);
+          navigate("/submit-your-art");
+        });
+      }
+    });
+  }
+
   return (
     <div id="submit">
       { 
         currentUser ? 
         <ProjectForm genres={genres} technologies={technologies} /> : 
-        <div>
-          <h1>Before you upload..</h1>
-          <Login setCurrentUser={setCurrentUser} users={users} />
+        <div id="before-uploading">
+          <h2>Please login before you upload your project..</h2>
+
+          <div className="login-signup-container">
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                autoComplete="off"
+                value={username}
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input type="submit" value="Login"/>
+            </form>
+          </div>
+          
         </div>
       }
     </div>
